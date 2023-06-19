6228769e74407a5a50944ae1211288e8d1be493a
Livia Oliveira <livia.deoliveira@live.com>
2023-06-19 13:57:07 -0300

    application entire project

---
+++ FilmesAPI/Data/Dtos/UpdateCinemaDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class UpdateCinemaDto
+    {
+        [Required(ErrorMessage = "O campo de nome e obrigatorio.")]
+        public string Nome { get; set; }
+    }
+}
